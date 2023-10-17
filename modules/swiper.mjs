0361ede3e39aa76b80873a51d7cfd2f8aae4eccf
Kate Glushkova <weirdis1@gmail.com>
2023-10-17 20:09:31 +0300

    3 slides per view

--- modules/swiper.mjs
+++ modules/swiper.mjs
@@ -1,22 +1,14 @@
 // core version + navigation, pagination modules:
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.mjs';
-// import { Navigation, Pagination } from 'https://cdn.jsdelivr.net/npm/swiper@10.3.1/+esm';
+//import { Navigation, Pagination } from 'https://cdn.jsdelivr.net/npm/swiper@10.3.1/+esm';
 
 const swiper = new Swiper('.swiper', {
     // Optional parameters
-    observeParents: true,
-    observer: true, 
-    direction: 'horizontal',
     slidesPerView: 3,
-    loop: true,
-    autoplay:{
-      delay: 5000,
-      disableOnInteraction: false
-    },
-  
-    // pagination
+      
     pagination: {
-      el: '.swiper-pagination',
+      el: ".swiper-pagination",
+      clickable: true,
     },
   
     // Navigation arrows
